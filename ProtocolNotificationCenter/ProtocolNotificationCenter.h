7f514248ec0107d6a2d3c937c884e39fdb0f6c6c
jayying007 <1172510964@qq.com>
2023-02-24 21:09:52 +0800

    Initial Commit

---
+++ ProtocolNotificationCenter/ProtocolNotificationCenter.h
@@ -0,0 +1,18 @@
+//
+//  ProtocolNotificationCenter.h
+//  ProtocolNotificationCenter
+//
+//  Created by janezhuang on 2023/2/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ProtocolNotificationCenter.
+FOUNDATION_EXPORT double ProtocolNotificationCenterVersionNumber;
+
+//! Project version string for ProtocolNotificationCenter.
+FOUNDATION_EXPORT const unsigned char ProtocolNotificationCenterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProtocolNotificationCenter/PublicHeader.h>
+
+
